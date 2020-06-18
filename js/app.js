a7df9428c36fe811e6a80c0a280f961fe0e1acbd
cmapereira <cmapereira@gmail.com>
2020-06-18 14:43:14 -0300

    logic finalized bugs solved

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
+const play = document.querySelector('#jogar');
+const table = document.querySelector('.table');
+const initial = document.querySelector('.initial');
 const cards = document.querySelectorAll('.card');
-
+let facil = document.querySelector('#facil');
+let medio = document.querySelector('#medio');
+let dificil = document.querySelector('#dificil');
+let numrounds = document.querySelector('#num-rounds');
 let hasFlipedCard = false;
 let firstCard, secondCard;
 
 let points = 0;
-let clicklimit = 0;
+let round = 120;
+let copyround = 120;
+
+console.log(numrounds);
+
+facil.addEventListener('click', defFacil);
+medio.addEventListener('click', defMedio);
+dificil.addEventListener('click', defDificil);
 
 let lockboard = false;
 
@@ -29,12 +42,16 @@ function hiddenCard(){
 function checkForMatch(){
    //cards match
    if(firstCard.dataset.bicho === secondCard.dataset.bicho){
+      points ++;
+      round --;
+      numrounds.textContent = round;
       firstCard.removeEventListner('click', hiddenCard);
       secondCard.removeEventListner('click', hiddenCard);
       resetBoard();
-
    //cards dont match
    } else {
+      round --;
+      numrounds.textContent = round;
       lockboard = true;
       setTimeout(() => {
          firstCard.lastElementChild.classList.remove('hidden');
@@ -49,12 +66,38 @@ function resetBoard(){
    [firstCard, secondCard] = [null, null];
 }
 
-(function shuffle(){
+function playGame(){
+   initial.classList.add('hidden');
+   table.classList.remove('hidden');
+   round = copyround;
+   numrounds.textContent = round;
+   points = 0;
    cards.forEach(card => {
       let randomPosition = Math.floor(Math.random()*36);
       card.style.order = randomPosition;
+      card.lastElementChild.classList.remove('hidden');
+      card.lastElementChild.classList.remove('hidden');
    })
-})();
+};
+
+function defFacil(){
+   round = 120;
+   copyround = 120;
+   numrounds.textContent = copyround;
+};
+
+function defMedio(){
+   round = 90;
+   copyround = 90;
+   numrounds.textContent = copyround;
+};
+
+function defDificil(){
+   round = 60;
+   copyround = 60;
+   numrounds.textContent = copyround;
+};
 
 cards.forEach(card => card.addEventListener('click', hiddenCard));
+play.addEventListener('click', playGame);
 
