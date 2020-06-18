63bb962764a7368bc135bbe1b1340626c5377d18
cmapereira <cmapereira@gmail.com>
2020-06-18 11:40:28 -0300

    logica  de cartas concluída

--- js/app.js
+++ js/app.js
@@ -3,7 +3,14 @@ const cards = document.querySelectorAll('.card');
 let hasFlipedCard = false;
 let firstCard, secondCard;
 
+let points = 0;
+let clicklimit = 0;
+
+let lockboard = false;
+
 function hiddenCard(){
+   if (lockboard) return;
+   if (this === firstCard) return;
    this.lastElementChild.classList.add('hidden');
    //this.classList.toggle('flip');
    //this.firstElementChild.classList.toggle('flipinv');
@@ -15,8 +22,39 @@ function hiddenCard(){
       //second click
       hasFlipedCard = false;
       secondCard = this;
+      checkForMatch();
    }  
 }
+
+function checkForMatch(){
+   //cards match
+   if(firstCard.dataset.bicho === secondCard.dataset.bicho){
+      firstCard.removeEventListner('click', hiddenCard);
+      secondCard.removeEventListner('click', hiddenCard);
+      resetBoard();
+
+   //cards dont match
+   } else {
+      lockboard = true;
+      setTimeout(() => {
+         firstCard.lastElementChild.classList.remove('hidden');
+         secondCard.lastElementChild.classList.remove('hidden');
+         resetBoard();
+      }, 1500)
+   }
+}
+
+function resetBoard(){
+   [hasFlipedCard, lockboard] = [false, false];
+   [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+   cards.forEach(card => {
+      let randomPosition = Math.floor(Math.random()*36);
+      card.style.order = randomPosition;
+   })
+})();
 
 cards.forEach(card => card.addEventListener('click', hiddenCard));
 
